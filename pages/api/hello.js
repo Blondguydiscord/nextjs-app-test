69650c3a2756d97387b3f7cadf11925ef2607379
JaoSasa <65865187+JaoSasa@users.noreply.github.com>
2021-06-02 21:57:25 +0100

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { slug } = req.query as { slug: string };
+};

488019066c152b0d42666178701f4020c9555e3c
JaoSasa <65865187+JaoSasa@users.noreply.github.com>
2021-06-02 21:51:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import HelloAPI from './api/hello.js'
 
 export default function Home() {
   return (
     <div classname="tittle">
-        <h1>Hello World</h1>
+        <h1>Hello, {res.name}</h1>
     </div>
   )
 }
